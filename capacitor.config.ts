8c44efd1d9218b498d38821df96d9fecd333bc08
Paul Hoerenz <development@hoerenz.com>
2024-06-03 14:24:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "memoria",
+  webDir: "dist",
+};
+
+export default config;
