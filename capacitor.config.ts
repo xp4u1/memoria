07332f4e8bf19d406e5e7bcf13aa08d70394e20c
Paul Hoerenz <development@hoerenz.com>
2024-06-19 21:39:47 +0200

    fix: update app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "de.xp4u1.discite",
   appName: "memoria",
   webDir: "dist",
 };
