cfa49c9235680f9726731026af0ef6571c2db3c8
Paul Hoerenz <development@hoerenz.com>
2024-06-21 14:46:24 +0200

    feat(ui): show correct greeting and current date
    Removed dummy data.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,55 @@ import { IonContent, IonPage } from "@ionic/react";
 import "./Home.scss";
 import WeekView from "../components/WeekView";
 
+const getGreeting = () => {
+  const hours = new Date().getHours();
+
+  if (hours >= 5 && hours < 11) return "guten morgen.";
+  if (hours >= 11 && hours < 18) return "guten tag.";
+
+  return "guten abend.";
+};
+
+const getDate = () => {
+  const today = new Date();
+
+  const days = [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+  ];
+  const months = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember",
+  ];
+
+  return `${days[today.getDay()]}, ${today.getDate()}. ${
+    months[today.getMonth()]
+  }`;
+};
+
 const Home: React.FC = () => {
   return (
     <IonPage id="homePage">
       <IonContent fullscreen>
         <div id="container">
           <header>
-            <h1>guten abend.</h1>
-            <h2>Sonntag, 2. Juni</h2>
+            <h1>{getGreeting()}</h1>
+            <h2>{getDate()}</h2>
           </header>
 
           <WeekView />
