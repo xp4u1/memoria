77d07528e23e9245e44dcba23f986cad877c3efe
Paul Hoerenz <development@hoerenz.com>
2024-08-08 15:19:37 +0200

    feat(i18n): add English and German translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-// TODO: translation files
+import de from "../locales/de/translation.json";
+import en from "../locales/en/translation.json";
+
 const resources = {
-  en: {
-    translation: {
-      "Current Language": "English",
-    },
-  },
   de: {
-    translation: {
-      "Current Language": "Deutsch",
-    },
+    translation: de,
+  },
+  en: {
+    translation: en,
   },
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
+  fallbackLng: "en",
   interpolation: {
     escapeValue: false,
   },
