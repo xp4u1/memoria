b5d1d04451125fc1b5ff7c3202e74fe3f6c1b6cf
Paul Hoerenz <development@hoerenz.com>
2024-10-15 20:50:52 +0200

    chore: fix formatting

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
 import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
+import { initReactI18next } from "react-i18next";
 
 import de from "../locales/de/translation.json";
 import en from "../locales/en/translation.json";
