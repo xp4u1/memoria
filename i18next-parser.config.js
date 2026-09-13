e0b7a11e2ed573644c180c32c2ba7ddfcbc392bc
Paul Hoerenz <development@hoerenz.com>
2026-09-13 21:40:23 +0200

    chore: upgrade dependencies (#31)
    - upgrade all dependencies
    - migrate to Capacitor 8.5
    - migrate to Ionic React 9
    - remove deprecated i18next-parser and migrate to i18next-cli
    - migrate Cocoapods to Swift Package Manager
    - update Github Workflows, migrate to pnpm/setup@v2 action

--- i18next-parser.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  keySeparator: false,
-  namespaceSeparator: false,
-  pluralSeparator: false,
-  locales: ["en", "de", "it", "tr"],
-  input: "src/**/*{ts,tsx}",
-  sort: true,
-};
